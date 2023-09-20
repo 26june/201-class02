252112113345a89ea282362cce8414a8ee033bf1
26june <gj.abordo1@gmail.com>
2023-09-20 10:40:40 +0100

    change ux flox

--- app.js
+++ app.js
@@ -1,108 +1,150 @@
 "use strict";
 
+let factsList;
+let documentScore;
+let documentButton;
+let iterationCount = 0;
 let scoreCounter = 0;
 
 function startQuiz() {
-  scoreCounter = 0;
-  let factsList = document.getElementById("facts-list");
-  factsList.style.filter = "blur(6px)";
+  switch (iterationCount) {
+    case 0:
+      scoreCounter = 0;
+      factsList = document.getElementById("facts-list");
+      factsList.style.filter = "blur(6px)";
+      documentScore = document.getElementById("score-counter");
+      documentButton = document.getElementById("quiz-button");
+      setTimeout(() => {
+        let q1Answered = false;
+        while (!q1Answered) {
+          let q1Answer = prompt(
+            "Am I 23 years old?",
+            `Please type "yes" or "no"`
+          );
+          switch (q1Answer.toLowerCase()) {
+            case "yes":
+              q1Answered = true;
+              scoreCounter++;
+              alert("Correct!");
+              break;
+            case "no":
+              q1Answered = true;
+              alert("Wrong!");
+              break;
+            default:
+              alert("Please type yes or no");
+          }
+        }
 
-  let documentScore = document.getElementById("score-counter");
+        documentButton.innerText = "Next Question";
+        documentScore.innerText = `Your current score is ${scoreCounter}/5!`;
+        iterationCount++;
+      }, 0);
+      break;
 
-  //defer the prompt
-  setTimeout(() => {
-    let q1Answered = false;
-    while (!q1Answered) {
-      let q1Answer = prompt("Am I 23 years old?", `Please type "yes" or "no"`);
-      switch (q1Answer.toLowerCase()) {
-        case "yes":
-          q1Answered = true;
-          scoreCounter++;
-          break;
-        case "no":
-          q1Answered = true;
-          break;
-        default:
-          alert("Please type yes or no");
+    case 1:
+      let q2Answered = false;
+      while (!q2Answered) {
+        let q2Answer = prompt(
+          "Did I go to Nottingham University?",
+          `Please type "yes" or "no"`
+        );
+        switch (q2Answer.toLowerCase()) {
+          case "yes":
+            q2Answered = true;
+            alert("Wrong!");
+            break;
+          case "no":
+            q2Answered = true;
+            scoreCounter++;
+            alert("Correct!");
+            break;
+          default:
+            alert("Please type yes or no");
+        }
       }
-    }
 
-    let q2Answered = false;
-    while (!q2Answered) {
-      let q2Answer = prompt(
-        "Did I go to Nottingham University?",
-        `Please type "yes" or "no"`
-      );
-      switch (q2Answer.toLowerCase()) {
-        case "yes":
-          q2Answered = true;
-          break;
-        case "no":
-          q2Answered = true;
-          scoreCounter++;
-          break;
-        default:
-          alert("Please type yes or no");
+      documentScore.innerText = `Your current score is ${scoreCounter}/5!`;
+      iterationCount++;
+      break;
+
+    case 2:
+      let q3Answered = false;
+      while (!q3Answered) {
+        let q3Answer = prompt("Was I previously a paramedic?");
+        switch (q3Answer.toLowerCase()) {
+          case "yes":
+            q3Answered = true;
+            alert("Wrong!");
+            break;
+          case "no":
+            q3Answered = true;
+            scoreCounter++;
+            alert("Correct!");
+            break;
+          default:
+            alert("Please type yes or no");
+        }
       }
-    }
 
-    let q3Answered = false;
-    while (!q3Answered) {
-      let q3Answer = prompt("Was I previously a paramedic?");
-      switch (q3Answer.toLowerCase()) {
-        case "yes":
-          q3Answered = true;
-          break;
-        case "no":
-          q3Answered = true;
-          scoreCounter++;
-          break;
-        default:
-          alert("Please type yes or no");
+      documentScore.innerText = `Your current score is ${scoreCounter}/5!`;
+      iterationCount++;
+      break;
+
+    case 3:
+      let q4Answered = false;
+      while (!q4Answered) {
+        let q4Answer = prompt("Is comedy my favourite genre of film?");
+        switch (q4Answer.toLowerCase()) {
+          case "yes":
+            q4Answered = true;
+            alert("Wrong!");
+            break;
+          case "no":
+            q4Answered = true;
+            scoreCounter++;
+            alert("Correct!");
+            break;
+          default:
+            alert("Please type yes or no");
+        }
       }
-    }
 
-    let q4Answered = false;
-    while (!q4Answered) {
-      let q4Answer = prompt("Is comedy my favourite genre of film?");
-      switch (q4Answer.toLowerCase()) {
-        case "yes":
-          q4Answered = true;
-          break;
-        case "no":
-          q4Answered = true;
-          scoreCounter++;
-          break;
-        default:
-          alert("Please type yes or no");
+      documentScore.innerText = `Your current score is ${scoreCounter}/5!`;
+      iterationCount++;
+      break;
+
+    case 4:
+      let q5Answered = false;
+      while (!q5Answered) {
+        let q5Answer = prompt("Am I a fan of Two Door Cinema Club?");
+        switch (q5Answer.toLowerCase()) {
+          case "yes":
+            q5Answered = true;
+            scoreCounter++;
+            alert("Correct!");
+            break;
+          case "no":
+            q5Answered = true;
+            alert("Wrong!");
+            break;
+          default:
+            alert("Please type yes or no");
+        }
       }
-    }
 
-    let q5Answered = false;
-    while (!q5Answered) {
-      let q5Answer = prompt("Am I a fan of Two Door Cinema Club?");
-      switch (q5Answer.toLowerCase()) {
-        case "yes":
-          q5Answered = true;
-          scoreCounter++;
-          break;
-        case "no":
-          q5Answered = true;
-          break;
-        default:
-          alert("Please type yes or no");
-      }
-    }
+      documentScore.innerText = `You scored ${scoreCounter}/5! ${
+        scoreCounter <= 2 ? "You dont know me at all!" : "You know me so well!"
+      }`;
 
-    // console.log(`Your score is: ${scoreCounter}/5`);
-    alert(`Your total score is ${scoreCounter}/5`);
+      documentButton.innerText = "Take the quiz again";
+      factsList.style.filter = "";
+      iterationCount = 0;
+      break;
 
-    documentScore.innerText = `You scored ${scoreCounter}/5! ${
-      scoreCounter <= 2 ? "You dont know me at all!" : "You know me so well!"
-    }`;
-
-    factsList.style.filter = "";
-  }, 0);
+    default:
+      break;
+  }
 }
 
 //TODO add carousel
